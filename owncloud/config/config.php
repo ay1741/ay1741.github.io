cadab9b22623235b599ec566ba199c2a98b27b74
ay1741 <ay1741@yahoo.com>
2018-01-10 00:03:04 -0500

    Data pulled from bluehost
    Data from site

---
+++ owncloud/config/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocy3zmfhtb3a',
+  'passwordsalt' => 'TdKCI6IAAm89oqpBaQ9Yahmgs2trjZ',
+  'secret' => 'tvk2YaTLFYYXWZdL1UCLmd4Qyn7UKhebqJksKCyChXqOyNxy',
+  'trusted_domains' => 
+  array (
+    0 => 'www.alexandertyoung.com',
+  ),
+  'datadirectory' => '/home2/alexapm9/public_html/owncloud/data',
+  'overwrite.cli.url' => 'http://www.alexandertyoung.com/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '8.0.0.7',
+  'dbname' => 'ownCloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'installed' => true,
+);
